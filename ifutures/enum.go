961256288b72ab2619249f0bae60bc21f520775e
msw <mswl@ya.ru>
2022-09-05 07:13:33 +0300

    ifutures

---
+++ ifutures/enum.go
@@ -0,0 +1,13 @@
+package ifutures
+
+//
+type Coin string
+
+const (
+	CoinBTC  Coin = "BTC"
+	CoinETH       = "ETH"
+	CoinEOS       = "EOS"
+	CoinXRP       = "XRP"
+	CoinUSDT      = "DOT"
+	CoinUSDT      = "USDT"
+)
---
+++ ifutures/response.go
@@ -0,0 +1,9 @@
+package ifutures
+
+type Response[T any] struct {
+	RetCode int    `json:"ret_code"`
+	RetMsg  string `json:"ret_msg"`
+	ExtCode string `json:"ext_code"`
+	ExtInfo string `json:"ext_info"`
+	Result  T      `json:"result"`
+}
