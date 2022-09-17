0d5bcd946008f2544e1cc70a8a6a2749a89a9828
msw <mswl@ya.ru>
2022-09-17 14:45:46 +0300

    uperpetual account active

---
+++ uperpetual/account.go
@@ -0,0 +1,10 @@
+// [Account Data Endpoints] https://bybit-exchange.github.io/docs/futuresV2/linear/#t-accountdata
+package uperpetual
+
+type PositionIdx int
+
+const (
+	OneWay   PositionIdx = 0
+	BuySide  PositionIdx = 1
+	SellSide PositionIdx = 1
+)
