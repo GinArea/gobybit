8291125b7d1a1e661e9c41623e701a4f947889af
msw <mswl@ya.ru>
2022-10-08 19:28:34 +0300

    iperpetual key info time

--- iperpetual/account_api.go
+++ iperpetual/account_api.go
@@ -1,6 +1,10 @@
 // API Key Info (https://bybit-exchange.github.io/docs/futuresV2/inverse/#t-key)
 package iperpetual
 
+import (
+	"time"
+)
+
 type GetKeyInfo struct {
 }
 
@@ -9,18 +13,18 @@ func (this GetKeyInfo) Do(client *Client) ([]KeyInfo, bool) {
 }
 
 type KeyInfo struct {
-	ApiKey        string   `json:"api_key"`
-	Type          string   `json:"type"`
-	UserID        int      `json:"user_id"`
-	InviterID     int      `json:"inviter_id"`
-	Ips           []string `json:"ips"`
-	Note          string   `json:"note"`
-	Permissions   []string `json:"permissions"`
-	CreatedAt     string   `json:"created_at"`
-	ExpiredAt     string   `json:"expired_at"`
-	ReadOnly      bool     `json:"read_only"`
-	VipLevel      string   `json:"vip_level"`
-	MktMakerLevel string   `json:"mkt_maker_level"`
+	ApiKey        string    `json:"api_key"`
+	Type          string    `json:"type"`
+	UserID        int       `json:"user_id"`
+	InviterID     int       `json:"inviter_id"`
+	Ips           []string  `json:"ips"`
+	Note          string    `json:"note"`
+	Permissions   []string  `json:"permissions"`
+	CreatedAt     time.Time `json:"created_at"`
+	ExpiredAt     time.Time `json:"expired_at"`
+	ReadOnly      bool      `json:"read_only"`
+	VipLevel      string    `json:"vip_level"`
+	MktMakerLevel string    `json:"mkt_maker_level"`
 }
 
 func (this *Client) GetKeyInfo() ([]KeyInfo, bool) {
