cea39c43ac180e7d318d37609334da54609cdd8d
msw <mswl@ya.ru>
2022-10-09 15:00:36 +0300

    JsonFloat64 for string representation

--- iperpetual/market.go
+++ iperpetual/market.go
@@ -1,39 +1,41 @@
 // Market Data Endpoints (https://bybit-exchange.github.io/docs/futuresV2/inverse/#t-marketdata)
 package iperpetual
 
+import "github.com/ginarea/gobybit/transport"
+
 // Query Symbol (https://bybit-exchange.github.io/docs/futuresV2/inverse/#t-querysymbol)
 type SymbolInfo struct {
-	Name            string         `json:"name"`
-	Alias           string         `json:"alias"`
-	Status          ContractStatus `json:"status"`
-	BaseCurrency    string         `json:"base_currency"`
-	QuoteCurrency   string         `json:"quote_currency"`
-	PriceScale      int            `json:"price_scale"`
-	TakerFee        string         `json:"taker_fee"`
-	MakerFee        string         `json:"maker_fee"`
-	FundingInterval int            `json:"funding_interval"`
-	LeverageFilter  LeverageFilter `json:"leverage_filter"`
-	PriceFilter     PriceFilter    `json:"price_filter"`
-	LotSizeFilter   LotSizeFilter  `json:"lot_size_filter"`
+	Name            string                `json:"name"`
+	Alias           string                `json:"alias"`
+	Status          ContractStatus        `json:"status"`
+	BaseCurrency    string                `json:"base_currency"`
+	QuoteCurrency   string                `json:"quote_currency"`
+	PriceScale      int                   `json:"price_scale"`
+	TakerFee        transport.JsonFloat64 `json:"taker_fee"`
+	MakerFee        transport.JsonFloat64 `json:"maker_fee"`
+	FundingInterval int                   `json:"funding_interval"`
+	LeverageFilter  LeverageFilter        `json:"leverage_filter"`
+	PriceFilter     PriceFilter           `json:"price_filter"`
+	LotSizeFilter   LotSizeFilter         `json:"lot_size_filter"`
 }
 
 type LeverageFilter struct {
-	Min  int    `json:"min_leverage"`
-	Max  int    `json:"max_leverage"`
-	Step string `json:"leverage_step"`
+	Min  int                   `json:"min_leverage"`
+	Max  int                   `json:"max_leverage"`
+	Step transport.JsonFloat64 `json:"leverage_step"`
 }
 
 type PriceFilter struct {
-	Min      string `json:"min_price"`
-	Max      string `json:"max_price"`
-	TickSize string `json:"tick_size"`
+	Min      transport.JsonFloat64 `json:"min_price"`
+	Max      transport.JsonFloat64 `json:"max_price"`
+	TickSize transport.JsonFloat64 `json:"tick_size"`
 }
 
 type LotSizeFilter struct {
-	MaxTradingQty         float64 `json:"max_trading_qty"`
-	MinTradingQty         float64 `json:"min_trading_qty"`
-	QtyStep               float64 `json:"qty_step"`
-	PostOnlyMaxTradingQty string  `json:"post_only_max_trading_qty"`
+	MaxTradingQty         float64               `json:"max_trading_qty"`
+	MinTradingQty         float64               `json:"min_trading_qty"`
+	QtyStep               float64               `json:"qty_step"`
+	PostOnlyMaxTradingQty transport.JsonFloat64 `json:"post_only_max_trading_qty"`
 }
 
 func (this *Client) QuerySymbol() ([]SymbolInfo, bool) {
--- transport/json.go
+++ transport/json.go
@@ -3,6 +3,8 @@ package transport
 import (
 	"encoding/json"
 	"fmt"
+	"strconv"
+	"strings"
 )
 
 func JsonUnmarshal[V any](jsonBlob []byte) V {
@@ -12,4 +14,18 @@ func JsonUnmarshal[V any](jsonBlob []byte) V {
 		panic(fmt.Sprint("json unmarshal: ", err))
 	}
 	return v
+}
+
+type JsonFloat64 float64
+
+func (this *JsonFloat64) UnmarshalJSON(b []byte) error {
+	s := string(b)
+	s = strings.Trim(s, `"`)
+	f, err := strconv.ParseFloat(s, 64)
+	*this = JsonFloat64(f)
+	return err
+}
+
+func (this *JsonFloat64) Value() float64 {
+	return float64(*this)
 }
