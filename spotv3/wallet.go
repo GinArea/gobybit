7266ab5345a385e13e96ef7eb604210c470af296
msw <mswl@ya.ru>
2023-02-22 10:45:11 +0300

    spotv3 wallet

--- spotv3/wallet.go
+++ spotv3/wallet.go
@@ -1,14 +1,16 @@
 // Wallet Data Endpoints (https://bybit-exchange.github.io/docs/spot/v3/#t-wallet)
 package spotv3
 
+import "github.com/ginarea/gobybit/transport"
+
 // Get Wallet Balance (https://bybit-exchange.github.io/docs/spot/v3/#t-balance)
 type Balance struct {
-	Coin     string `json:"coin"`
-	CoinID   string `json:"coinId"`
-	CoinName string `json:"coinName"`
-	Total    string `json:"total"`
-	Free     string `json:"free"`
-	Locked   string `json:"locked"`
+	Coin     string            `json:"coin"`
+	CoinID   string            `json:"coinId"`
+	CoinName string            `json:"coinName"`
+	Total    transport.Float64 `json:"total"`
+	Free     transport.Float64 `json:"free"`
+	Locked   string            `json:"locked"`
 }
 
 func (this *Client) WalletBalance() ([]Balance, error) {
