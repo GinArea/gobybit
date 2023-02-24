c75174ff1a441f27307707147eca554ec4d26d5e
msw <mswl@ya.ru>
2023-02-24 17:54:18 +0300

    spotv3 ws subscription

---
+++ spotv3/ws_subscription.go
@@ -0,0 +1,20 @@
+package spotv3
+
+import "github.com/msw-x/moon/ufmt"
+
+type Subscription struct {
+	Topic    TopicName
+	Interval string
+	Symbol   string
+}
+
+func (o *Subscription) String() string {
+	s := []string{string(o.Topic)}
+	if o.Interval != "" {
+		s = append(s, o.Interval)
+	}
+	if o.Symbol != "" {
+		s = append(s, o.Symbol)
+	}
+	return ufmt.JoinSliceWith(".", s)
+}
